15f9cff70ae2c193eb40c70ca5032033f5645613
Ahmed98AM <heroahmed486@yahoo.com>
2021-11-28 20:27:31 +0200

    adding user routes

---
+++ routes/userRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const userController = require("../controllers/userController");
+
+const router = express.Router();
+router.route("/").get(userController.getAllUsers);
+router
+  .route("/:id")
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+export default router;
